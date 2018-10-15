a8cf8fa199a68cddb5e883a6b4e75577a3210ea8
David Conlan <david.conlan@gmail.com>
2018-10-15 15:10:31 +1000

    Initial commit

---
+++ SimpleOntologyExternalModule.php
@@ -0,0 +1,343 @@
+<?php
+/**
+ *
+CSIRO Open Source Software Licence Agreement (variation of the BSD / MIT License)
+Copyright (c) 2018, Commonwealth Scientific and Industrial Research Organisation (CSIRO) ABN 41 687 119 230.
+All rights reserved. CSIRO is willing to grant you a licence to this SimpleOntologyExternalModule on the following terms, except where otherwise indicated for third party material.
+Redistribution and use of this software in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* Neither the name of CSIRO nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission of CSIRO.
+EXCEPT AS EXPRESSLY STATED IN THIS AGREEMENT AND TO THE FULL EXTENT PERMITTED BY APPLICABLE LAW, THE SOFTWARE IS PROVIDED "AS-IS". CSIRO MAKES NO REPRESENTATIONS, WARRANTIES OR CONDITIONS OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO ANY REPRESENTATIONS, WARRANTIES OR CONDITIONS REGARDING THE CONTENTS OR ACCURACY OF THE SOFTWARE, OR OF TITLE, MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, THE ABSENCE OF LATENT OR OTHER DEFECTS, OR THE PRESENCE OR ABSENCE OF ERRORS, WHETHER OR NOT DISCOVERABLE.
+TO THE FULL EXTENT PERMITTED BY APPLICABLE LAW, IN NO EVENT SHALL CSIRO BE LIABLE ON ANY LEGAL THEORY (INCLUDING, WITHOUT LIMITATION, IN AN ACTION FOR BREACH OF CONTRACT, NEGLIGENCE OR OTHERWISE) FOR ANY CLAIM, LOSS, DAMAGES OR OTHER LIABILITY HOWSOEVER INCURRED.  WITHOUT LIMITING THE SCOPE OF THE PREVIOUS SENTENCE THE EXCLUSION OF LIABILITY SHALL INCLUDE: LOSS OF PRODUCTION OR OPERATION TIME, LOSS, DAMAGE OR CORRUPTION OF DATA OR RECORDS; OR LOSS OF ANTICIPATED SAVINGS, OPPORTUNITY, REVENUE, PROFIT OR GOODWILL, OR OTHER ECONOMIC LOSS; OR ANY SPECIAL, INCIDENTAL, INDIRECT, CONSEQUENTIAL, PUNITIVE OR EXEMPLARY DAMAGES, ARISING OUT OF OR IN CONNECTION WITH THIS AGREEMENT, ACCESS OF THE SOFTWARE OR ANY OTHER DEALINGS WITH THE SOFTWARE, EVEN IF CSIRO HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH CLAIM, LOSS, DAMAGES OR OTHER LIABILITY.
+APPLICABLE LEGISLATION SUCH AS THE AUSTRALIAN CONSUMER LAW MAY APPLY REPRESENTATIONS, WARRANTIES, OR CONDITIONS, OR IMPOSES OBLIGATIONS OR LIABILITY ON CSIRO THAT CANNOT BE EXCLUDED, RESTRICTED OR MODIFIED TO THE FULL EXTENT SET OUT IN THE EXPRESS TERMS OF THIS CLAUSE ABOVE "CONSUMER GUARANTEES".  TO THE EXTENT THAT SUCH CONSUMER GUARANTEES CONTINUE TO APPLY, THEN TO THE FULL EXTENT PERMITTED BY THE APPLICABLE LEGISLATION, THE LIABILITY OF CSIRO UNDER THE RELEVANT CONSUMER GUARANTEE IS LIMITED (WHERE PERMITTED AT CSIRO'S OPTION) TO ONE OF FOLLOWING REMEDIES OR SUBSTANTIALLY EQUIVALENT REMEDIES:
+(a)               THE REPLACEMENT OF THE SOFTWARE, THE SUPPLY OF EQUIVALENT SOFTWARE, OR SUPPLYING RELEVANT SERVICES AGAIN;
+(b)               THE REPAIR OF THE SOFTWARE;
+(c)               THE PAYMENT OF THE COST OF REPLACING THE SOFTWARE, OF ACQUIRING EQUIVALENT SOFTWARE, HAVING THE RELEVANT SERVICES SUPPLIED AGAIN, OR HAVING THE SOFTWARE REPAIRED.
+IN THIS CLAUSE, CSIRO INCLUDES ANY THIRD PARTY AUTHOR OR OWNER OF ANY PART OF THE SOFTWARE OR MATERIAL DISTRIBUTED WITH IT.  CSIRO MAY ENFORCE ANY RIGHTS ON BEHALF OF THE RELEVANT THIRD PARTY.
+Third Party Components
+The following third party components are distributed with the Software.  You agree to comply with the licence terms for these components as part of accessing the Software.  Other third party software may also be identified in separate files distributed with the Software.
+ * 
+ * 
+ * 
+ */
+
+namespace AEHRC\SimpleOntologyExternalModule;
+
+use ExternalModules\AbstractExternalModule;
+use ExternalModules\ExternalModules;
+
+class SimpleOntologyExternalModule extends AbstractExternalModule  implements \OntologyProvider{
+
+  public function __construct() {
+      parent::__construct();
+      // register with OntologyManager
+      $manager = \OntologyManager::getOntologyManager();
+      $manager->addProvider($this);
+  }
+
+  public function redcap_every_page_before_render (int $project_id ){
+  }
+
+  public function validateSettings($settings){
+     $errors='';
+    
+     // make sure category has no markup or ' " char
+     $siteCategory = $settings['site-category'];
+     foreach($siteCategory as $category){
+        if ($category != strip_tags($category) 
+            || strpos($category, "'") !== false
+            || strpos($category, '"') !== false
+            ){
+          $errors .= "Category has illigal characters - ".$category."\n";
+        }
+      }
+     $projectCategory = $settings['project-category'];
+     foreach($projectCategory as $category){
+        if ($category != strip_tags($category) 
+             || strpos($category, "'") !== false
+            || strpos($category, '"') !== false
+             ){
+          $errors .= "Category has illigal characters - ".$category."\n";
+        }
+      }
+     // make sure name has no markup
+     foreach($settings['site-name'] as $name){
+        if ($name != strip_tags($name)){
+          $errors .= "Name has illigal characters - ".$name."\n";
+        }
+      }
+     foreach($settings['project-name'] as $name){
+        if ($name != strip_tags($name)){
+          $errors .= "Name has illigal characters - ".$name."\n";
+        }
+      }
+
+     // If value is json make sure its valid json and is an array
+     $values = $settings['site-values'];
+     foreach($settings['site-values-type'] as $key=>$valueType){
+        if ($valueType == 'json'){
+          // check json is valid
+          $rawValue = $values[$key];
+		      $list = json_decode($rawValue);
+          if (is_null($list)){
+             $errors .= "Invalid JSon [".$siteCategory[$key]. "] : " .json_last_error_msg()."\n";
+          }
+          else if (!is_array($list)){
+             $errors .= "Invalid JSon : Expected Array of objects\n";
+          }
+        }
+      }
+     $values = $settings['project-values'];
+     foreach($settings['project-values-type'] as $key=>$valueType){
+        if ($valueType == 'json'){
+          // check json is valid
+          $rawValue = $values[$key];
+		      $list = json_decode($rawValue);
+          if (is_null($list)){
+             $errors .= "Invalid JSon [".$projectCategory[$key]. "] : " .json_last_error_msg()."\n";
+          }
+          else if (!is_array($list)){
+             $errors .= "Invalid JSon : Expected Array of objects\n";
+          }
+        }
+      }
+     return $errors;
+  }
+
+  /**
+    * return the name of the ontology service as it will be display on the service selection
+    * drop down.
+    */
+  public function getProviderName(){
+    return 'Site Defined Ontologies';
+  }
+    
+
+  /**
+    return the prefex used to denote ontologies provided by this provider.
+   */
+  public function getServicePrefix(){
+    return 'SIMPLE';
+  }
+
+  function getSystemCategories() {
+      $key = 'site-category-list';
+      $keys = [ 'site-category' => 'category',
+                'site-name' => 'name',
+                'site-values-type' => 'values-type',
+                'site-values' => 'values' ];
+      $subSettings = [];
+      $rawSettings = $this->getSubSettings($key);
+      //error_log("system_settings = ".print_r($rawSettings, TRUE));
+      foreach($rawSettings as $data){
+        $subSetting = [];
+        foreach($keys as $k=>$nk){
+          $subSetting[$nk] = $data[$k];
+        }
+        $subSettings[] = $subSetting;
+      }
+      return $subSettings;
+    }
+
+  function getProjectCategories() {
+      $key = 'project-category-list';
+      $keys = [ 'project-category' => 'category',
+                'project-name' => 'name',
+                'project-values-type' => 'values-type',
+                'project-values' => 'values' ];
+      $subSettings = [];
+      $rawSettings = $this->getSubSettings($key);
+      //error_log("project_settings = ".print_r($rawSettings, TRUE));
+      foreach($rawSettings as $data){
+        $subSetting = [];
+        foreach($keys as $k=>$nk){
+          $subSetting[$nk] = $data[$k];
+        }
+        $subSettings[] = $subSetting;
+      }
+      return $subSettings;
+    }
+
+  /**
+    * Return a string which will be placed in the online designer for
+    * selecting an ontology for the service.
+    * When an ontology is selected it should make a javascript call to 
+    * update_ontology_selection($service, $category)
+    *
+    * The provider may include a javascript function
+    * <service>_ontology_changed(service, category)
+    * which will be called when the ontology selection is changed. This function
+    * would update any UI elements is the service matches or clear the UI elemements
+    * if they do not.
+    */
+  public function getOnlineDesignerSection(){
+    $systemCategories = $this->getSystemCategories();
+    $projectCategories = $this->getProjectCategories();
+
+    $categories = [];
+    foreach ($systemCategories as $cat){
+      $categories[$cat['category']] = $cat;
+    }
+    foreach ($projectCategories as $cat){
+      $categories[$cat['category']] = $cat;
+    }
+
+    $categoryList = '';
+    foreach ($categories as $cat){
+      $category = $cat['category'];
+      $name = $cat['name'];
+      $categoryList .= "<option value='{$category}'>{$name}</option>\n";
+    }
+
+   $onlineDesignerHtml = <<<EOD
+<script type="text/javascript">
+  function SIMPLE_ontology_changed(service, category){
+    var newSelection = ('SIMPLE' == service) ? category : '';
+    $('#simple_ontology_category').val(newSelection);
+  }
+  
+</script>
+<div style='margin-bottom:3px;'>
+  Select Local Ontology to use:
+</div>
+<select id='simple_ontology_category' name='simple_ontology_category' 
+            onchange="update_ontology_selection('SIMPLE', this.options[this.selectedIndex].value)"
+            class='x-form-text x-form-field' style='width:330px;max-width:330px;'>
+        {$categoryList}
+</select>
+EOD;
+    return $onlineDesignerHtml;
+  }
+
+	/**
+	 * Search API with a search term for a given ontology
+	 * Returns array of results with Notation as key and PrefLabel as value.
+	 */
+	public function searchOntology($category, $search_term, $result_limit){
+    $systemCategories = $this->getSystemCategories();
+    $projectCategories = $this->getProjectCategories();
+    $categories = [];
+    foreach ($systemCategories as $cat){
+      $categories[$cat['category']] = $cat;
+    }
+    foreach ($projectCategories as $cat){
+      $categories[$cat['category']] = $cat;
+    }
+
+    $values = array();
+    $categoryData = $categories[$category];
+    if ($categoryData){
+      $type = $categoryData['values-type'];
+      $rawValues = $categoryData['values'];
+      
+
+      if ($type == 'list'){
+        $list = preg_split("/\r\n|\n|\r/", $rawValues);
+        foreach($list as $item){
+          $values[] = ['code'=>$item,'display'=>$item];
+        }
+      }
+      elseif ($type == 'bar'){
+        $rows = preg_split("/\r\n|\n|\r/", $rawValues);
+        foreach($rows as $row){
+          $cols = explode('|', $row); 
+          $values[] = ['code'=>$cols[0],'display'=>$cols[1]];
+        }
+      }
+      elseif ($type == 'json'){
+		     $list = json_decode($rawValues, true);
+		     if (is_array($list)){
+            foreach($list as $item){
+              if (isset($item['code']) and isset($item['display'])){
+                $values[] = ['code'=>$item['code'],'display'=>$item['display']];
+              }
+           }
+        }
+      }
+    }
+    //error_log(print_r($values, TRUE));
+    // we want matches that start with search term before matches containing search term
+    $startResults = array();
+    $containsResults = array();
+    foreach($values as $val){
+      $code = $val['code'];
+      $desc = $val['display'];
+      $pos = stripos($desc, $search_term);
+      if ($pos !== FALSE){
+        if ($pos == 0){
+          $startResults[] = $val;
+        }
+        else {
+          $containsResults[] = $val;
+        }
+      }
+    }
+    $mresults = array_merge($startResults, $containsResults);
+    $results = array();
+    foreach($mresults as $val){
+      // make sure result is escaped..
+      $code = \REDCap::escapeHtml($val['code']);
+      $desc = \REDCap::escapeHtml($val['display']);
+      $results[$code] = $desc;
+    }
+
+    $result_limit = (is_numeric($result_limit) ? $result_limit : 20);
+		// Return array of results
+		return array_slice($results, 0, $result_limit, true);
+  }
+
+
+  /**
+   *  Takes the value and gives back the label for the value.
+   */
+  public function getLabelForValue($category, $value){
+    $systemCategories = $this->getSystemCategories();
+    $projectCategories = $this->getProjectCategories();
+    $categories = [];
+    foreach ($systemCategories as $cat){
+      $categories[$cat['category']] = $cat;
+    }
+    foreach ($projectCategories as $cat){
+      $categories[$cat['category']] = $cat;
+    }
+
+    $values = array();
+    $categoryData = $categories[$category];
+    if ($categoryData){
+      $type = $categoryData['values-type'];
+      $rawValues = $categoryData['values'];
+      
+
+      if ($type == 'list'){
+        $list = preg_split("/\r\n|\n|\r/", $rawValues);
+        foreach($list as $item){
+          $values[] = ['code'=>$item,'display'=>$item];
+        }
+      }
+      elseif ($type == 'bar'){
+        $rows = preg_split("/\r\n|\n|\r/", $rawValues);
+        foreach($rows as $row){
+          $cols = explode('|', $row); 
+          $values[] = ['code'=>$cols[0],'display'=>$cols[1]];
+        }
+      }
+      elseif ($type == 'json'){
+		     $list = json_decode($rawValues, true);
+		     if (is_array($list)){
+            foreach($list as $item){
+              if (isset($item['code']) and isset($item['display'])){
+                $values[] = ['code'=>$item['code'],'display'=>$item['display']];
+              }
+           }
+        }
+      }
+      if (array_key_exists($value, $values)){
+        return $values[$value];
+      }
+    }
+    return $value;
+  }
+
+  
+}
